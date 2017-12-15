a2c32cecd52748aefb62d62480504310a1dfd2ab
Xueql <1285958162@qq.com>
2017-12-15 14:39:18 +0800

    the home page about category

--- app/components/HomeHeader/index.js
+++ app/components/HomeHeader/index.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+
 import './style.less'
 
 class Homeheader extends React.Component {
+	constructor(props) {
+		super(props)
+		this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdat
+	}
+
 	render() {
 		return (
-			<div id='home-header'>
-				<div className="home-header-left">
-					<span>beijing</span>
-					<i className="iconfont icon-arrow-down"></i>
+			<div id='home-header' className='clear-fix'>
+				<div className="home-header-left float-left">
+					<span>北京</span>
+					&nbsp;
+					<i className="iconfont icon-arrow-down-new"></i>
+				</div>
+				<div className="home-header-right float-right">
+					<i className="iconfont icon-user"></i>
+				</div>
+				<div className="home-header-middle">
+					<div className="search-container">
+						<i className="iconfont icon-seach"></i>
+						<input type="text" placeholder='请输入关键字'/>
+					</div>
 				</div>
 			</div>
 		)
--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
+
 import HomeHeader from '../../components/HomeHeader/'
+import Category from '../../components/Category/'
 
 class Home extends React.Component {
 	render() {
 		return (
 			<div>
 				<HomeHeader />
+				<Category />
 			</div>
 		)
 	}
